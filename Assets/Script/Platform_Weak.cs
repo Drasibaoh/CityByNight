d739f14073acb036a48024e0480f06685fbf7852
choppEX <victorderay@gmail.com>
2021-04-14 16:31:47 +0200

    Ajout du timer + début de plateforme qui se brise

---
+++ Assets/Script/Platform_Weak.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform_Weak : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Collision");
+        if (other.CompareTag("Player"))
+        {
+            Invoke("Disappear", 1.5f);
+        }
+    }
+
+    public void Disappear()
+    {
+       this.gameObject.SetActive(false);
+    }
+}
+
+            
+        
---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float timer = 0.0f;
+    public float maxtimer = 10.0f;
+    public float actualtimer = 0.0f;
+    public bool Timsup = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = maxtimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Timsup == false)
+        {
+            timer -= 1 * Time.deltaTime;
+        }
+        
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Livraison1"))
+        {
+            actualtimer = timer;
+            timer = maxtimer + actualtimer;
+        }
+        if (other.CompareTag("Livraison2"))
+        {
+            actualtimer = timer;
+            timer = maxtimer + actualtimer;
+        }
+        if (other.CompareTag("Livraison3"))
+        {
+            Timsup = true;
+            if (timer <= 0)
+            {
+                Debug.Log("Lose");
+            }
+            else if(timer > 0)
+            {
+                Debug.Log("Win");
+            }
+        }
+    }
+}
