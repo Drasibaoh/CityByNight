7dd0b4415a9ba6a7e2a5a839edf5a5378e221c2d
Drasibaoh <71836998+Drasibaoh@users.noreply.github.com>
2021-05-21 16:19:13 +0200

    killPlain

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Death.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.FirstPerson
+{
+    public class Death : MonoBehaviour
+    {
+        ControllerAddon player;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                if (player==null)
+                    player = other.GetComponent<ControllerAddon>();
+                player.Death();
+            }
+        }
+    }
+}
