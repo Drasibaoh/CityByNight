c4c6bb1c498f26be623fcf656a26e317f957dac8
Drasibaoh <71836998+Drasibaoh@users.noreply.github.com>
2021-05-26 12:04:50 +0200

    ajouts de la V1 des crédits

--- Assets/Script/MenuNavigation.cs
+++ Assets/Script/MenuNavigation.cs
@@ -3,8 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 public class MenuNavigation : MonoBehaviour
 {
+    public VideoPlayer cred;
+    public RawImage image;
+    bool hasStarted;
+    private void Start()
+    {
+        if(cred!=null)
+            cred.Prepare();
+
+    }
+    private void Update()
+    {
+        if (hasStarted)
+        {
+            if (!cred.isPlaying)
+            {
+                cred.Prepare();
+                image.gameObject.SetActive(false);
+            }
+        }
+    }
     public void ReturnToMenu()
     {
         SceneManager.LoadScene(0);
@@ -20,6 +41,12 @@ public class MenuNavigation : MonoBehaviour
     public void Win()
     {
         SceneManager.LoadScene(2);
+    }
+    public void Credits()
+    {
+        image.gameObject.SetActive(true);
+        cred.Play();
+        hasStarted = true;
     }
     public void Lose()
     {
